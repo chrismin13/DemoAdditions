0576decc8072d12817164f7f2da1e507dd72c1f7
chrismin13 <chrismin1302@gmail.com>
2017-07-15 19:48:45 +0300

    Comments

--- src/com/chrismin13/demoadditions/Items.java
+++ src/com/chrismin13/demoadditions/Items.java
@@ -17,7 +17,7 @@ public class Items implements Listener { // Class must implement Listener
 
 	@EventHandler
 	public void onInitialization(AdditionsAPIInitializationEvent event) {
-		// Adds the Resource Pack from the plugin's jar.
+		// Adds the Resource Pack from the plugin's jar. Don't forget to include the .zip extendsion!
 		event.addResourcePackFromPlugin(DemoAdditions.instance, "DemoAdditions-v1.0.zip");
 
 		// Adds a new Powered Obsidian Sword to be initialized.
